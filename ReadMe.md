3718e2122779a477f40c4143db93862216d5bce2
kaveh <kavehmollaei@gmail.com>
2020-01-10 11:05:23 +0330

    قسمت اول پروژه کلیک کردن در drop down list

--- ReadMe.md
+++ ReadMe.md
@@ -3,3 +3,5 @@ web scarping
 this is my progect
 web scraping ccc selector
 selenium
+پروژه دوره وب اسکردینگ
+ part 1
--- webscraping-pro-maktab.py
+++ webscraping-pro-maktab.py
@@ -1,5 +1,6 @@
 from  selenium import webdriver
 import selenium
+import datetime
 
 
 
@@ -8,9 +9,9 @@ driver.maximize_window()
 
 
 url = 'https://www.seleniumeasy.com/test/basic-first-form-demo.html'
-
+# get url
 driver.get(url)
-#driver.get(url)
+
 username_field = driver.find_element_by_id('user-message')
 username_field.clear() 
 username_field.send_keys('kavehmollaei@gmail.com')
@@ -26,5 +27,13 @@ input_field2 = driver.find_element_by_id('sum2')
 input_field2.send_keys('3')
 push_button2 = driver.find_element_by_xpath('//*[(@id = "gettotal")]//*[contains(concat( " ", @class, " " ), concat( " ", "btn-default", " " ))]')
 push_button2.click()
-#driver.close()
+
+url_url = 'https://www.seleniumeasy.com/test/basic-select-dropdown-demo.html'
+url_url_url = 'http://demo.guru99.com/test/newtours/register.php'
+driver.get(url_url)
+x = datetime.datetime.now()
+day_hafteh = x.strftime('%A')
+select_dropdownbox = driver.find_element_by_xpath("//*[(@id = 'select-demo')]/option[text()='%s']" %day_hafteh).click() 
+
+driver.close()
 
