f8c7384e3f4079dd10998e4cc9386cbfaaa1a71e
kaveh <kavehmollaei@gmail.com>
2020-01-10 21:51:45 +0330

    کتابخانه request و beatiful soap

--- webscraping-cssselector.py
+++ webscraping-cssselector.py
@@ -2,10 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 
 
-url = 'https://www.gajmarket.com/'
+url = 'https://en.wikipedia.org/wiki/Nineteen_Eighty-Four'
 r = requests.get(url)
+css_selector = '.searchaux+ .navigation-not-searchable'
+
 
 soup = BeautifulSoup(r.text,'html.parser')
-title_soup = soup.find_all('.slick-slide')
+#title_soup = soup.find_all('.slick-slide')
 
-print(title_soup)
+title_soup = soup.select(css_selector)
+print(title_soup[0].get_text())
+
+
+
+#استخراج به کمک css_selector
